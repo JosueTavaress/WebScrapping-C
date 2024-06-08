b523db07bd0e2c473e9ebc43c92b159b45a82285
JosueTavaress <josueferreira8.jf@gmail.com>
2024-06-08 18:34:38 -0300

    controller (wip): create script scrapping

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using HtmlAgilityPack;
+using System.Xml.Linq;
+using System.Web;
 
 namespace WebScrapping_C.Controllers;
 
@@ -21,6 +24,54 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
+        int page = 1;
+        List<Item> items = new List<Item>();
+        bool hasNextPage = true;
+
+
+        while (hasNextPage)
+        {
+            var web = new HtmlWeb();
+            HtmlDocument document = web.Load($"https://www.tbca.net.br/base-dados/composicao_estatistica.php?pagina={page}&atuald=1");
+            var nodesList = document.DocumentNode.SelectNodes("//table/tbody/tr");
+
+
+            if (nodesList != null)
+            {
+
+                foreach(var node in nodesList)
+                {
+                    List<string> arr = new List<string>();
+                    foreach (var item in node.ChildNodes)
+                    {
+                        string encodedData = item.InnerText;
+                        string decodedData = HttpUtility.HtmlDecode(encodedData);
+                        arr.Add(decodedData);
+                    }
+                    var nodes = arr.ToArray();
+                    var objItem = new Item();
+                    objItem.Code = nodes[0];
+                    objItem.Name = nodes[1];
+                    objItem.ScientificName = nodes[2];
+                    objItem.Group = nodes[3];
+                    objItem.Brand = nodes[4];
+                    items.Add(objItem);
+
+                }
+                page++;
+            }
+            else {
+                hasNextPage = false;
+            }
+        }
+
+       
+        foreach (var item in items)
+        {
+            Console.WriteLine($"Code: {item.Code}, Name: {item.Name}, ScientificName: {item.ScientificName}, Group: {item.Group}, Brand: {item.Brand}");
+        }
+       
+
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateTime.Now.AddDays(index),
---
+++ Item.cs
@@ -0,0 +1,19 @@
+﻿namespace WebScrapping_C;
+
+public class Item
+{
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public string ScientificName { get; set; }
+    public string Group { get; set; }
+    public string Brand { get; set; }
+
+    public Item()
+    {
+        Code = string.Empty;
+        Name = string.Empty;
+        ScientificName = string.Empty;
+        Group = string.Empty;
+        Brand = string.Empty;
+    }
+}
