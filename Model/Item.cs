f14ee247a8907a4c3aadcf63f1f1dd60bc7a7193
JosueTavaress <josueferreira8.jf@gmail.com>
2024-06-11 08:39:35 -0300

    Properties (feat): map relationship item

---
+++ Model/Item.cs
@@ -0,0 +1,21 @@
+﻿namespace WebScrapping_C.Model;
+
+public class Item
+{
+    public int Id { get; set; }
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public string ScientificName { get; set; }
+    public string Group { get; set; }
+    public string Brand { get; set; }
+    public List<Properties> Details { get; set; } = new List<Properties>();
+
+    public Item()
+    {
+        Code = string.Empty;
+        Name = string.Empty;
+        ScientificName = string.Empty;
+        Group = string.Empty;
+        Brand = string.Empty;
+    }
+}
---
+++ Model/Properties.cs
@@ -0,0 +1,32 @@
+﻿namespace WebScrapping_C.Model
+{
+    public class Properties
+    {
+        public int Id { get; set; }
+        public string Units { get; set; }
+        public string ValuePer100G { get; set; }
+        public string StandardDeviation { get; set; }
+        public string MinimumValue { get; set; }
+        public string MaximumValue { get; set; }
+        public string NumberOfDataUsed { get; set; }
+        public string References { get; set; }
+        public string DataType { get; set; }
+        public string Component { get; set; }
+
+        public int ItemId { get; set; } // forem_key (entity)
+        public Item Item { get; set; } // nav - item
+
+        public Properties()
+        {
+            Component = string.Empty;
+            Units = string.Empty;
+            ValuePer100G = string.Empty;
+            StandardDeviation = string.Empty;
+            MinimumValue = string.Empty;
+            MaximumValue = string.Empty;
+            NumberOfDataUsed = string.Empty;
+            References = string.Empty;
+            DataType = string.Empty;
+        }
+    }
+}
