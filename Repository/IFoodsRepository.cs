1e5fc55514f458fb070e321918a2a1ff65a052b2
JosueTavaress <josueferreira8.jf@gmail.com>
2024-06-11 08:47:31 -0300

    IFoods (feat): create interface food

---
+++ Repository/IFoodsRepository.cs
@@ -0,0 +1,12 @@
+﻿using WebScrapping_C.Model;
+
+namespace WebScrapping_C.Repository
+{
+    public interface IFoodsRepository
+    {
+        Task<IEnumerable<Item>> GetItemsAsync();
+        Task CreateItemAsync(Item item);
+        Task<bool> SaveChangesAsync();
+    }
+}
+
