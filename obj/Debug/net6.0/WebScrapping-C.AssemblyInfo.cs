aa6c0fa0129dc490edb0a04dc88ee96db81bc3f7
JosueTavaress <josueferreira8.jf@gmail.com>
2024-06-11 09:14:23 -0300

    bin (refactor): add bin

--- obj/Debug/net6.0/WebScrapping-C.AssemblyInfo.cs
+++ obj/Debug/net6.0/WebScrapping-C.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebScrapping-C")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+074fd836f316a269827c4ff3a280ee2b63dd0429")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ab7e21517308a73615871852156c27ac95aca03")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebScrapping-C")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebScrapping-C")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
