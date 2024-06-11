4c8c5ae730d2ce7355ebe92fee6d976a9e7f299f
JosueTavaress <josueferreira8.jf@gmail.com>
2024-06-11 08:41:05 -0300

    obj (feat): add obj

--- obj/Release/net6.0/WebScrapping-C.AssemblyInfo.cs
+++ obj/Release/net6.0/WebScrapping-C.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebScrapping-C")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+074fd836f316a269827c4ff3a280ee2b63dd0429")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebScrapping-C")]
