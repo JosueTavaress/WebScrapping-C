f08d5fcdc1324d1ffa1b608c782040b5d2f108db
JosueTavaress <josueferreira8.jf@gmail.com>
2024-06-11 08:48:06 -0300

    Food (feat): implement class

---
+++ Repository/FoodsRepository.cs
@@ -0,0 +1,31 @@
+﻿using WebScrapping_C.Data;
+using WebScrapping_C.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebScrapping_C.Repository
+{
+    public class FoodsRepository : IFoodsRepository
+    {
+        private readonly FoodsContex context;
+
+        public FoodsRepository(FoodsContex context)
+        {
+            this.context = context;
+        }
+
+        public async Task CreateItemAsync(Item item)
+        {
+            await this.context.AddAsync(item);
+        }
+
+        public async Task<IEnumerable<Item>> GetItemsAsync()
+        {
+            return await this.context.Items.ToListAsync();
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return await this.context.SaveChangesAsync() > 0;
+        }
+    }
+}
