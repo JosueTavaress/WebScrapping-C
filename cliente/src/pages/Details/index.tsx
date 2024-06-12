d9a8e379bfd4a52402714d35c71e074e36acd2ae
JosueTavaress <josueferreira8.jf@gmail.com>
2024-06-12 14:46:13 -0300

    Details (feat): create page details

--- cliente/src/pages/Details/index.tsx
+++ cliente/src/pages/Details/index.tsx
@@ -1,5 +1,69 @@
+import { useEffect, useState } from 'react';
+import { getDetailsFood } from '../../api/http-foods/request';
+import { useParams } from 'react-router-dom';
+import { ChakraProvider, Box, Table, Thead, Tbody, Tr, Th, Td, Container } from '@chakra-ui/react';
+import { IResponseDetails } from '../../api/http-foods/interface';
+
 const Details = () => {
-  return <p>Details</p>
+  const [details, setDetails] = useState<IResponseDetails[]>([]);
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const getDetails = async (code: string) => {
+      console.log('Fetching details for ID:', code);
+      const data = await getDetailsFood(code);
+      setDetails(data);
+      setLoading(false);
+      console.log('Details fetched:', data);
+    } 
+    if (id) {
+      getDetails(id);
+    }
+  }, [id]);
+
+  if (loading) return <p>Loading...</p>; // wip refactor component
+
+  return (
+      <ChakraProvider>
+        <Container maxW="container.xl" p={4}>
+          <Box overflowX="auto">
+            <Table variant="striped" colorScheme="teal">
+              <Thead>
+                <Tr>
+                  <Th>Componente</Th>
+                  <Th>Unidades</Th>
+                  <Th>Valor por 100 g</Th>
+                  <Th>Desvio Padrão</Th>
+                  <Th>Valor Mínimo</Th>
+                  <Th>Valor Máximo</Th>
+                  <Th>Número de dados utilizados</Th>
+                  <Th>Referências</Th>
+                  <Th>Tipo de dados</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {details.map((element, indx) => {
+                  return (
+                    <Tr key={indx}>
+                      <Td>{element.component}</Td>
+                      <Td>{element.units}</Td>
+                      <Td>{element.valuePer100G}</Td>
+                      <Td>{element.standardDeviation}</Td>
+                      <Td>{element.minimumValue}</Td>
+                      <Td>{element.maximumValue}</Td>
+                      <Td>{element.numberOfDataUsed}</Td>
+                      <Td>{element.references}</Td>
+                      <Td>{element.dataType}</Td>
+                    </Tr>
+                  )
+                })}
+              </Tbody>
+            </Table>
+          </Box>
+        </Container>
+      </ChakraProvider>
+    );
 }
 
 export default Details;
